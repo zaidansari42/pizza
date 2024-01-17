b8e7695ab2e3f2300f74e62baf328866e0025402
Zaid Ansari <zaidansari42@gmail.com>
2024-01-17 13:21:17 +0530

    removed unsused elements & added Pizza data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import pizzaData from "./data";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <div>App</div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
